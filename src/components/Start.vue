94f95bfeec165718d897f7a161bf41031e022898
Ilya <mordkovich.ilya@googlemail.com>
2018-09-05 22:15:13 +0200

    scss and css checkbox

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider"></span>
+    </label>
   </div>
 </template>
 
@@ -18,9 +22,71 @@ export default {
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped >
 h1 {
   color: #42b983;
 }
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+
+  .input {
+    display:none;
+  }
+}
+
+
+
+.slider {
+  border-radius: 34px;
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+
+  &:before {
+    border-radius: 50%;
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+
+}
+
+input:checked {
+  + .slider {
+    background-color: #2196F3;
+  }
+}
+
+input:focus {
+  + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+}
+
+
+input:checked {
+  + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+}
+
+
 </style>
