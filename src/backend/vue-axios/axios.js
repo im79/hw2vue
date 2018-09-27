66b2b68c36364e0d8903cc9c628a2309db47c080
Ilya <mordkovich.ilya@googlemail.com>
2018-09-27 21:33:47 +0200

    env

--- src/backend/vue-axios/axios.js
+++ src/backend/vue-axios/axios.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://authserver'
+const API_URL = process.env.VUE_APP_API_URL  || 'http://authserver'
 
 export default axios.create({
   baseURL: API_URL,
